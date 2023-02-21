3eb0f2a582d6e37c794997f1cdcb16f07de41ddb
okcanvas <okcanvas@naver.com>
2023-02-21 11:59:49 +0900

    first

--- android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModule.java
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModule.java
@@ -403,7 +403,8 @@ class PickerModule extends ReactContextBaseJavaModule implements ActivityEventLi
         setConfiguration(options);
         resultCollector.setup(promise, multiple);
 
-        permissionsCheck(activity, promise, Collections.singletonList(Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU ? android.Manifest.permission.WRITE_EXTERNAL_STORAGE : android.Manifest.permission.READ_MEDIA_IMAGES), new Callable<Void>() {
+        // permissionsCheck(activity, promise, Collections.singletonList(Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU ? android.Manifest.permission.WRITE_EXTERNAL_STORAGE : android.Manifest.permission.READ_MEDIA_IMAGES), new Callable<Void>() {
+        permissionsCheck(activity, promise, Collections.singletonList(Build.VERSION.SDK_INT < Build.VERSION_CODES.S ? android.Manifest.permission.WRITE_EXTERNAL_STORAGE : android.Manifest.permission.READ_MEDIA_IMAGES), new Callable<Void>() {
             @Override
             public Void call() {
                 initiatePicker(activity);
